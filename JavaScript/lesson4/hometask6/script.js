ebb5da2e22548d077b0966c55f0d74888ca06fd6
Roman Hrynko <romanhrynkoua@gmail.com>
2021-04-19 19:56:04 +0300

    lesson4

--- JavaScript/lesson4/hometask6/script.js
+++ JavaScript/lesson4/hometask6/script.js
@@ -1,7 +1,7 @@
 let sum = 0;
-for (let i = 0; i <= 1000; i += 1) {
+for (let i = 1; i <= 1000; i += 1) {
   sum += i;
 }
-const result1 = Math.floor(sum / 1234);
+const result1 = sum / 1234;
 const result2 = sum % 1234;
 console.log(result1 > result2);
